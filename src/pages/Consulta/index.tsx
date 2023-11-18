40b5551f61c5ed95688001992983d8f4da443bee
Carlos Lins <carloslinsalbertoa@gmail.com>
2023-11-18 07:59:36 -0300

    Adição de uma screen de consulta para modularização e resolver problema de build

--- src/pages/Consulta/index.tsx
+++ src/pages/Consulta/index.tsx
@@ -1,49 +1,12 @@
-import { Header } from "@/components/Header";
 import Navbar from "@/components/Navbar";
-import {
-  ConsultaBodyConsultasContainer,
-  ConsultaBodyContainer,
-  ConsultaContainer,
-  Consultas,
-} from "./styles";
+import ConsultaContainer from "./styles";
+import { ConsultaScreen } from "@/screens/ConsultaScreen";
 
 function Consulta() {
   return (
     <ConsultaContainer>
       <Navbar />
-      <ConsultaBodyContainer>
-        <Header />
-        <ConsultaBodyConsultasContainer>
-          <Consultas>
-            <p>Requerimento 1</p>
-            <p>07/10/2023 08:30PM</p>
-          </Consultas>
-          <Consultas>
-            <p>Requerimento 2</p>
-            <p>07/10/2023 08:30PM</p>
-          </Consultas>
-          <Consultas>
-            <p>Requerimento 3</p>
-            <p>07/10/2023 08:30PM</p>
-          </Consultas>
-          <Consultas>
-            <p>Requerimento 4</p>
-            <p>07/10/2023 08:30PM</p>
-          </Consultas>
-          <Consultas>
-            <p>Requerimento 5</p>
-            <p>07/10/2023 08:30PM</p>
-          </Consultas>
-          <Consultas>
-            <p>Requerimento 6</p>
-            <p>07/10/2023 08:30PM</p>
-          </Consultas>
-          <Consultas>
-            <p>Requerimento 7</p>
-            <p>07/10/2023 08:30PM</p>
-          </Consultas>
-        </ConsultaBodyConsultasContainer>
-      </ConsultaBodyContainer>
+      <ConsultaScreen />
     </ConsultaContainer>
   );
 }
--- src/pages/Consulta/styles.ts
+++ src/pages/Consulta/styles.ts
@@ -4,32 +4,4 @@ const ConsultaContainer = styled.section`
   display: flex;
 `;
 
-const ConsultaBodyContainer = styled.section`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-`;
-
-const ConsultaBodyConsultasContainer = styled.section`
-  padding: 60px 5px;
-  display: flex;
-  flex-direction: column;
-  gap: 28px;
-`;
-
-const Consultas = styled.section`
-  border-radius: 5px;
-  background: #d9d9d9;
-  padding: 20px 24px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  cursor: pointer;
-`;
-
-export {
-  ConsultaContainer,
-  ConsultaBodyContainer,
-  ConsultaBodyConsultasContainer,
-  Consultas,
-};
+export default ConsultaContainer;
